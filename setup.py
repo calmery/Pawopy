3426546fb5efbb77eb6d498b8644639c3a5ef38a
Calmery <contact@calmery.me>
2017-04-23 02:50:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='Pawopy',
-    version='0.0.1',
+    version='0.0.2',
     description='A Python wrapper for the Mastodon API',
     packages=['pawopy'],
     install_requires=['requests'],
     url='https://github.com/calmery/Pawopy',
-    author='Marei Kikukawa',
+    author='calmery',
     author_email='contact@calmery.me',
     license='MIT',
     keywords='mastodon api mstdn pawoo'
